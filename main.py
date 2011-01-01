a90fc8d972e1e9d289ce47e1120503c84ed46627
Alec Perkins <alecperkins@gmail.com>
2011-01-01 16:33:47 -0500

    Moved the inbound message handler out of main so that it didn't require admin login (oops!)

--- main.py
+++ main.py
@@ -10,58 +10,6 @@ import settings
 
 from utils import render
 
-# https://github.com/simonw/json-time/blob/master/jsontime.py
-import re
-# Allow 'abc' and 'abc.def' but not '.abc' or 'abc.'
-valid_callback = re.compile('^\w+(\.\w+)*$')
-
-class MessageHandler(webapp.RequestHandler):
-    def get(self):
-        result = {'status': 'failed'}
-        raw_sender_ip = self.request.remote_addr
-        sender_ip = IP.get_or_insert(raw_sender_ip)
-        
-        if not sender_ip.blocked:
-            site = self.request.get('site', None)
-            sender = self.request.get('sender', None)
-            subject = self.request.get('subject', None)
-            body = self.request.get('body', None)
-            referrer = self.request.referrer
-            if not referrer:
-                referrer = self.request.referer
-        
-            site = Site.all().filter('domain =', site).fetch(1)
-            if len(site) == 1:
-                site = site[0]
-            else:
-                site = None
-        
-            if site and sender and subject and body:
-                message = Message(site=site, sender=sender, subject=subject, body=body, sender_ip=sender_ip, referrer=referrer)
-                message.put()
-                if site.dispatch and site.recipients:
-                    taskqueue.add(url='/tasks/dispatch/', params={ 'message_id': str(message.key()) })
-                result = {'status': 'success'}
-                sender_ip.count += 1
-            else:
-                sender_ip.blocked_count += 1
-        else:
-            sender_ip.blocked_count += 1
-
-        if not sender_ip.ip:
-            sender_ip.ip = raw_sender_ip
-
-        sender_ip.put()
-        if not sender_ip.geo and settings.IPINFODB_KEY:
-            taskqueue.add(url='/tasks/geolocate/', params={ 'ip_id': str(sender_ip.key()) })
-
-        self.response.headers["Content-Type"] = "application/javascript"
-        callback = self.request.get('callback', None)
-        if callback and valid_callback.match(callback):
-            result = '%s(%s)' % (callback, result)
-        self.response.out.write(result)
-
-
 class MessageBrowser(webapp.RequestHandler):
     def get(self):
         user = users.get_current_user()
@@ -124,7 +72,6 @@ class SiteBrowser(webapp.RequestHandler):
         
 def main():
     application = webapp.WSGIApplication([
-        ('/inbound/', MessageHandler),
         ('/', MessageBrowser),
         ('/blacklist/', BlacklistBrowser),
         ('/blacklist/add', BlacklistAdd),
